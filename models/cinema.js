c8a94321c0d12b347cc2de1faef65cf0ce351d7d
David Murphy <daveru@Davids-MacBook-Pro.local>
2020-10-14 15:25:40 +0100

    Initial commit

---
+++ models/cinema.js
@@ -0,0 +1,24 @@
+const Cinema = function (films) {
+  this.films = films;
+};
+
+
+Cinema.prototype.getTitles = function() { 
+  return this.films.map((film) => {
+    return film.title
+  });
+};
+
+Cinema.prototype.findByTitle = function(findTitle) {
+  return this.films.find((film) => {
+    return findTitle === film.title;
+  });
+};
+
+Cinema.prototype.filterByGenre = function(genreToFind) {
+  return this.films.filter((film) =>{
+    return genreToFind === film.genre;
+  });
+}
+
+module.exports = Cinema;
